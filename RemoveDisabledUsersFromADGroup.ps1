4236a4eb9507ccd45b5dec491254bdeb5f6185a1
OlegShmyrin <oleg.shmyrin@gmail.com>
2021-07-27 15:55:59 +0300

    Add files via upload

---
+++ RemoveDisabledUsersFromADGroup.ps1
@@ -0,0 +1,21 @@
+﻿$PSGroups = Get-ADGroup -Filter {name -like "grpAKZ-ps*"}
+
+  foreach($PSGroup in $PSGroups) {
+            
+            Write-host "Ищем откл пользователей в группе $PSGroup" -ForegroundColor Green
+
+            $grpusers = Get-ADGroupMember -Identity $PSGroup | ? {$_.objectclass -eq "user"}
+
+
+
+
+            foreach($user in $grpusers){
+
+    
+                $DisUser = Get-ADUser $user -Properties Name,Enabled | ? {$_.Enabled -eq $false} | Select-Object Name,Enabled   
+
+                $DisUser
+                #Remove-ADGroupMember -Identity $PSGroup -Members $DisUser -Verbose
+            }
+
+}
